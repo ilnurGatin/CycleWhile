ada634c3869fee05eafe60d5f5facacaec023901
ilnurGatin <reilag@yandex.ru>
2022-09-26 19:38:23 +0500

    Homework is done

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,10 @@ public class Main {
         int total = 0;
         int i = 0;
         while (total < 2_459_000) {
-            total +=salary;
+            total += salary;
             i++;
-            System.out.println("Месяц " + i + " сумма накоплений равна " + total + " рублей");
         }
+        System.out.println("Месяц " + i + " сумма накоплений равна " + total + " рублей");
         //Task #1.2
         i = 1;
         while (i <= 10) {
@@ -21,12 +21,14 @@ public class Main {
         for (int j = 10; j > 0; j--) {
             System.out.print(j + " ");
         }
+        System.out.println();
+
         //Task #1.3
         int population = 12_000_000;
-        int birthAmount = 17;
-        int dieAmount = -8;
-        for (i = 0; i <= 10; i++) {
-            population += (birthAmount + dieAmount) * population / 1000;
+        float birthAmount = 17;
+        float dieAmount = -8;
+        for (i = 1; i <= 10; i++) {
+            population += Math.round((birthAmount + dieAmount)/1000 * population);
             System.out.println("Год " + i + " численность населения составляет " + population);
         }
         //Task #2.1
@@ -35,18 +37,18 @@ public class Main {
         int month = 0;
         while (saving < 12_000_000) {
             saving += saving * percentPerMonth / 100;
-            month +=1;
+            month += 1;
             System.out.println("Месяц " + month + " накопления будут " + saving);
         }
         System.out.println("Всего месяцев ждать " + month);
 
         //Task #2.2
-        percentPerMonth = 7;
+        int newPercentPerMonth = 7;
         saving = 15_000;
         month = 0;
         while (saving < 12_000_000) {
-            saving += saving * percentPerMonth / 100;
-            month +=1;
+            saving += saving * newPercentPerMonth / 100;
+            month += 1;
             if (month % 6 == 0) {
                 System.out.println("Месяц " + month + " накопления будут " + saving);
             }
@@ -54,12 +56,12 @@ public class Main {
         System.out.println("Всего месяцев ждать " + month);
 
         //Task #2.3
-        int monthsPlaning = 9 * 12;
-        month = 0;
+        int yearsPlanning = 9;
+        int monthsPlaning = yearsPlanning * 12;
         saving = 15000;
-        while (month < monthsPlaning) {
+        for (month = 1; month <= monthsPlaning; month++) {
             saving += saving * percentPerMonth / 100;
-            month +=1;
+
             if (month % 6 == 0) {
                 System.out.println("Сумма накоплений за " + month + " месяцев составляет " + saving + " рублей");
             }
@@ -71,7 +73,7 @@ public class Main {
         int daysInWeek = 7;
         int daysInMonth = 31;
         for (int fridayDate = 4; fridayDate <= daysInMonth; fridayDate += daysInWeek) {
-            System.out.println("Сегодня пятница, " + fridayDate +"-е число. Необходимо подготовить отчет.");
+            System.out.println("Сегодня пятница, " + fridayDate + "-е число. Необходимо подготовить отчет.");
         }
 
         // Task 3.1
@@ -85,10 +87,9 @@ public class Main {
         //Task 3.2
         int num = 2;
         for (int k = 1; k <= 10; k++) {
-            System.out.println(num + "*" + k +"=" + num*k);
+            System.out.println(num + "*" + k + "=" + num * k);
         }
 
-        // Task #3.3
 
 
     }
